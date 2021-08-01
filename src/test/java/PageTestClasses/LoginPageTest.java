1c2c0afea702415a28cb4f476c7ad8002ee8cb6e
Naveen Kumar <naveenautomationrepository@gmail.com>
2021-08-01 17:13:22 +0530

    Added 5 Secs Wait

--- src/test/java/PageTestClasses/LoginPageTest.java
+++ src/test/java/PageTestClasses/LoginPageTest.java
@@ -7,9 +7,10 @@ import PageClasses.LoginPage;
 public class LoginPageTest extends LoginPage {
 
 	@Test
-	public static void loginPageMethodExecution() {
+	public static void loginPageMethodExecution() throws InterruptedException {
 		
 		loginPageMethod();
+		Thread.sleep(5000);
 	}
 	
 	
